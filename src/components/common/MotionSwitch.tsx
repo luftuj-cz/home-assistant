f89ea2260be7ef4cbdef582ad2aef146bc0ae998
Jaroslav Kaňka <kanka@kankaj.cz>
2026-02-03 10:40:49 +0100

    Create MotionSwitch
     (probably will be removed later)

---
+++ src/components/common/MotionSwitch.tsx
@@ -0,0 +1,39 @@
+import { Switch, type SwitchProps } from "@mantine/core";
+import { motion } from "framer-motion";
+import { forwardRef } from "react";
+
+export const MotionSwitch = forwardRef<HTMLInputElement, SwitchProps>((props, ref) => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.95 }}
+      initial={false}
+      animate={{
+        scale: props.checked ? 1.03 : 1,
+      }}
+      transition={{
+        type: "spring",
+        stiffness: 500,
+        damping: 15,
+        bounce: 0.6,
+      }}
+      style={{
+        display: "inline-flex",
+        width: "fit-content",
+        verticalAlign: "middle",
+      }}
+    >
+      <Switch
+        ref={ref}
+        {...props}
+        styles={{
+          thumb: {
+            transition: "transform 400ms cubic-bezier(0.68, -0.6, 0.32, 1.6)", // CSS bounce for the thumb movement
+          },
+        }}
+      />
+    </motion.div>
+  );
+});
+
+MotionSwitch.displayName = "MotionSwitch";
